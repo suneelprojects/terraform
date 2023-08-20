6d681d68eb6fb62ddcefbbe31e47df0f42240238
telugudevopsguru <140139079+telugudevopsguru@users.noreply.github.com>
2023-08-21 00:13:23 +0530

    Update main.tf

--- script/main.tf
+++ script/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAWIBQPDIZY7BZFS5G"
-  secret_key = "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"
 }
 resource "aws_instance" "instance-1" {
 ami = "ami-04d29b6f966df1537"
