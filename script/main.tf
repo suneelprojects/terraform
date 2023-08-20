90f7a5cc8bee939029403be33cb2ec724cfadae3
telugudevopsguru <140139079+telugudevopsguru@users.noreply.github.com>
2023-08-21 00:16:20 +0530

    Update main.tf

--- script/main.tf
+++ script/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
 region = "us-east-1"
-}
+access_key = var.ACCESS_KEY
+  secret_key = VAR.SECRET_KEY
 resource "aws_instance" "instance-1" {
 ami = "ami-04d29b6f966df1537"
 instance_type = "t2.nano"
